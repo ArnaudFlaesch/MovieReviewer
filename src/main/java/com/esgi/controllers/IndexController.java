4bb954869c7904ee3574626c47d17b63d1ff8bf0
Arnaud Flaesch <arnaudflaesch@hotmail.com>
2016-04-02 15:27:07 +0200

    Mise à jour du schéma de la base de données et ajout du contrôleur de la page d'accueil

---
+++ src/main/java/com/esgi/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.esgi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String getListMovies(Model model) {
+
+        return ("index");
+    }
+}
