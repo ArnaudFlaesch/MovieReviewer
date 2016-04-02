4560518c8114bacbd0800d089238f44cab087040
hideo <p.garciapelayo@gmail.com>
2016-04-02 17:35:10 +0200

    UserREpo

---
+++ src/main/java/com/esgi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.esgi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by hideo on 02/04/16.
+ */
+public class UserRepository extends JpaRepository<User,Long>{
+}
