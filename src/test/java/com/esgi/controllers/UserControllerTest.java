90cec9a78e8fec416b4f6b0487577570daf54652
hideo <p.garciapelayo@gmail.com>
2016-04-16 19:47:14 +0200

    Tests failed

---
+++ src/test/java/com/esgi/controllers/UserControllerTest.java
@@ -0,0 +1,38 @@
+package com.esgi.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hideo on 16/04/16.
+ */
+public class UserControllerTest {
+
+    @Test
+    public void create() throws Exception {
+
+
+    }
+
+    @Test
+    public void getUser() throws Exception {
+
+    }
+
+    @Test
+    public void removeById() throws Exception {
+
+    }
+
+    @Test
+    public void authenticateUser() throws Exception {
+
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+
+    }
+
+}
